0dd934a8bc1fdc8beb94da81653b83cb854b3da6
dev13company <avinash.thatikonda@t-systems.com>
2026-01-22 20:01:34 +0530

    commit

--- bwcm/src/sanity/schemaTypes/index.ts
+++ bwcm/src/sanity/schemaTypes/index.ts
@@ -1,7 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import heroSection from './heroSection';
 import galleryImage from './galleryImage';
+import meetingSection from './meetingSection';
+import aboutUsSection from "./aboutUsSection";
+import testimonialSection from './testimonialSection';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [heroSection, galleryImage],
+  types: [heroSection, galleryImage, meetingSection, aboutUsSection, testimonialSection],
 }
--- bwcm/src/sanity/structure.ts
+++ bwcm/src/sanity/structure.ts
@@ -3,13 +3,15 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
-    .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
-      S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
-      ),
-    ])
+    .title('Content')
+    .items(S.documentTypeListItems())
+    // .items([
+    //   S.documentTypeListItem('galleryImage').title('Gallery'),
+    //   S.documentTypeListItem('heroSection').title('Hero'),
+    //   S.documentTypeListItem('aboutUsSection').title('About Us'),
+
+    //   S.divider(),
+    //   ...S.documentTypeListItems().filter(
+    //     (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+    //   ),
+    // ])
